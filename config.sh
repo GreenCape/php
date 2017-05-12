265bc1e837a0c8e2c8c885c944790d60e08eafee
Niels Braczek <nbraczek@bsds.de>
2017-05-12 19:45:54 +0200

    Trying more explicit test configuration

--- config.sh
+++ config.sh
@@ -3,7 +3,11 @@
 source ~/official-images/test/config.sh
 
 testAlias+=(
-	["greencape/php"]='php'
-	["greencape/php:apache"]='php:apache'
-	["greencape/php:fpm"]='php:fpm'
+	["greencape/php:7.0"]='php'
+	["greencape/php:7.0-alpine"]='php'
+	["greencape/php:7.0-apache"]='php:apache'
+	["greencape/php:7.0-fpm"]='php:fpm'
+	["greencape/php:7.0-fpm-alpine"]='php:fpm'
+	["greencape/php:7.0-zts"]='php'
+	["greencape/php:7.0-zts-alpine"]='php'
 )
